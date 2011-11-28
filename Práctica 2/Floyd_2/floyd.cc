cde5555540da0c31a7d17fe0b2fd7a3b5c226914
Olmo Jiménez <olmojial@gmail.com>
2011-11-28 22:18:33 +0100

    Floyd bidimensional casi terminado

--- Práctica 2/Floyd_2/floyd.cc
+++ Práctica 2/Floyd_2/floyd.cc
@@ -4,6 +4,7 @@
 #include "Graph.h"
 #include "mpi.h"
 #include <math.h>
+#include <cstdio>
 using namespace std;
 
 //**************************************************************************
@@ -15,13 +16,11 @@ int main (int argc, char *argv[]){
 	int nverts, tam;
 	Graph G;
 	int *grafo;
+	int *solucion;
 	
 	MPI_Init(&argc,&argv);
 	MPI_Comm_size(MPI_COMM_WORLD, &size);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-	MPI_Status status;
-	MPI_Comm COMM_CART;
 
 	if (argc != 2) {
 		cerr << "Sintaxis: " << argv[0] << " <archivo de grafo>" << endl;
@@ -30,18 +29,7 @@ int main (int argc, char *argv[]){
 	}
 	
 	int raiz_p = sqrt(size);
-	
-	int dimensiones[] = {raiz_p, size/raiz_p}; //{filas comunicador, columnas comunicador}
-	int periodos[] = {0, 0};//No sera periodica ninguna dimension.
-
-	MPI_Cart_create(MPI_COMM_WORLD, 2, dimensiones, periodos, true, &COMM_CART);
-	
-	int coord[2];
-	
 	int rank_hor, rank_ver;
-	
-	
-	double t=MPI_Wtime();
 
 	if(rank==0){
 		G.lee(argv[1]);
@@ -79,6 +67,7 @@ int main (int argc, char *argv[]){
 	}
 	
 	MPI_Bcast(&tam, 1, MPI_INT, 0, MPI_COMM_WORLD);
+	MPI_Bcast(&nverts, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	
 	buf_recepcion = new int[tam*tam];
 
@@ -86,51 +75,89 @@ int main (int argc, char *argv[]){
 	MPI_Scatter(buf_envio, tam*tam, MPI_INT, buf_recepcion, tam*tam, MPI_INT, 0, MPI_COMM_WORLD);
 	
 	//Un comunicador por cada fila y columna
-	MPI_Comm horizontal, vertical;
-	MPI_Comm_split(MPI_COMM_WORLD, rank/tam, 0, comm_horizontal);
-	MPI_Comm_split(MPI_COMM_WORLD, rank%tam, 0, comm_vertical);
+	MPI_Comm comm_horizontal, comm_vertical;
+	MPI_Comm_split(MPI_COMM_WORLD, rank/tam, 0, &comm_horizontal);
+	MPI_Comm_split(MPI_COMM_WORLD, rank%tam, 0, &comm_vertical);
 	
 	MPI_Comm_rank(comm_horizontal, &rank_hor);
 	MPI_Comm_rank(comm_vertical, &rank_ver);
+	
+	int dims[] = {raiz_p, raiz_p};
+    int periods[] = {0, 0};
+    MPI_Comm COMM_CART;
+    int rank_cart;
+    int coord[2];
+	
+	MPI_Cart_create(MPI_COMM_WORLD , 2, dims, periods, true, &COMM_CART);
+	MPI_Comm_rank(COMM_CART, &rank_cart);
 
 	// BUCLE PPAL DEL ALGORITMO
 	double t=MPI_Wtime();
 	
 	int *filak = new int[tam];
-	int *columna = new int[tam];
+	int *columnak = new int[tam];
 	int i,j,k,vikj;
 	
+	int fila_f = rank/raiz_p*tam;
+	int columna_f = rank%raiz_p*tam;
+	
+	int aux, arriba, abajo, izq, der;
+	
 	for(k=0;k<nverts;k++){
-		if(rank == k%tam){
-			for (int i=0; i<tam; i++)
-				filak[i] = buf_recepcion[k%tam*tam+i];
-				
-			for (int i=0; i<tam; i++)
-				columnak[i] = buf_recepcion[i*tam+k%tam];
+		for(i=0; i<raiz_p; i++){
+			coord[0] = k/tam; coord[1] = i;
+			MPI_Cart_rank(COMM_CART, coord, &aux);
+			if(rank_cart==aux){
+				for (int l=0; l<tam; l++)
+					filak[l] = buf_recepcion[k%tam*tam+l];
+			}
+			MPI_Bcast(filak, tam, MPI_INT, k/tam, comm_vertical);
 		}
 		
-		MPI_Bcast(filak, tam, MPI_INT, k%tam, comm_vertical);
-		MPI_Bcast(columnak, tam, MPI_INT, k%tam, comm_horizontal);
+		for(i=0; i<raiz_p; i++){
+			coord[0] = i; coord[1] = k/tam;
+			MPI_Cart_rank(COMM_CART, coord, &aux);
+			if(rank_cart==aux){
+				for (int l=0; l<tam; l++)
+					columnak[l] = buf_recepcion[k%tam+l*tam];
+			}
+			MPI_Bcast(columnak, tam, MPI_INT, k/tam, comm_horizontal);
+		}
 		
-		for(i=0;i<nverts;i++)
-			for(j=0;j<nverts;j++)
-				if (i!=j && i!=k && j!=k){
-					vikj=G.arista(i,k)+G.arista(k,j);
-					vikj=min(vikj,G.arista(i,j));
-					G.inserta_arista(i,j,vikj);   
+		for(i=0;i<tam;i++)
+			for(j=0;j<tam;j++)
+				if (i+fila_f!=j+columna_f && i+fila_f!=k && j+columna_f!=k){
+					vikj = columnak[i]+filak[j];
+					vikj = min(vikj, buf_recepcion[i*tam+j]);
+					buf_recepcion[i*tam+j] = vikj;
 				}
+	}
+	
+	solucion = new int[nverts*nverts];
+	MPI_Datatype bloque2;
+	MPI_Type_vector(tam, tam, nverts, MPI_INT, &bloque2);
+	MPI_Type_commit(&bloque2);
+	
+	MPI_Gather(buf_recepcion, tam*tam, MPI_INT, grafo, tam*tam, MPI_INT, 0, MPI_COMM_WORLD);
+	
+	int posicion=0, fila_p, columna_p, comienzo;
+	for (int i=0; i<size; i++){
+		fila_p = i/raiz_p;
+		columna_p = i%raiz_p;
+		comienzo = (columna_p*tam)+(fila_p*tam*tam*raiz_p);
+		
+		MPI_Unpack(grafo, sizeof(int)*nverts*nverts, &posicion, &solucion[comienzo], 1, bloque2, MPI_COMM_WORLD);
 	}
 
 	t=MPI_Wtime()-t;
 
 	if(rank==0){
 		cout << endl<<"EL Grafo con las distancias de los caminos más cortos es:" << endl;
+		G.A = solucion;
 		G.imprime();
 		cout<< "Tiempo gastado= " << t << endl;
 	}
 	
 	MPI_Finalize();
 }
-
-
 
