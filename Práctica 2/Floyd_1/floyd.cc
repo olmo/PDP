71bba1966afbed80948d59296b0bd54cda7197f8
Olmo Jiménez <olmojial@gmail.com>
2011-11-30 21:27:52 +0100

    Floyd1 comentado

--- Práctica 2/Floyd_1/floyd.cc
+++ Práctica 2/Floyd_1/floyd.cc
@@ -41,12 +41,15 @@ int main (int argc, char *argv[]){
 		grafo = G.A;
 	}
 	
+	//Transmitimos a todos los procesos el número de vértices
 	MPI_Bcast(&nverts, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	
 	int comienzo = 0;
 	int partes = nverts / size;
 	int aux = nverts%size;
 	
+	//Calculamos cuantas filas tiene que calcular cada proceso
+	//y cual es la fila inicial de cada uno con respecto a la matriz total
 	if(aux>rank){
 		partes++;
 		comienzo = rank*partes;
@@ -56,9 +59,11 @@ int main (int argc, char *argv[]){
 	}
 	
 	//Repartimos la matriz entre los procesos
+	//Cada proceso tendrá su submatriz "particion"
 	int *particion = new int[nverts*partes];
 	MPI_Scatter(grafo, partes*nverts, MPI_INT, particion, partes*nverts, MPI_INT, 0, MPI_COMM_WORLD);
 	
+	//Creamos el vector donde se almacenará la fila k
 	int *filak = new int[nverts];
 	int vijk;
 	
@@ -73,7 +78,9 @@ int main (int argc, char *argv[]){
 			
 		MPI_Bcast(filak, nverts, MPI_INT, k/partes, MPI_COMM_WORLD);
 		
-		for(int i=0; i<partes && i<nverts; i++){
+		//Cada proceso recorre solo las filas que tiene en su submatriz (particion)
+		//"comienzo" indica la fila inicial de cada proceso con respecto a la matriz total
+		for(int i=0; i<partes; i++){
 			for (int j=0; j<nverts; j++){
 				if (comienzo+i!=j && comienzo+i!=k && j!=k){
 					vijk = particion[i*nverts+k] + filak[j];
@@ -84,9 +91,10 @@ int main (int argc, char *argv[]){
 		}
 	}
 	
-	MPI_Gather(particion, partes*nverts, MPI_INT, grafo, partes*nverts, MPI_INT, 0, MPI_COMM_WORLD);
-	
 	t=MPI_Wtime()-t;
+	
+	//Obtenemos en el proceso 0 la información calculada por cada proceso
+	MPI_Gather(particion, partes*nverts, MPI_INT, grafo, partes*nverts, MPI_INT, 0, MPI_COMM_WORLD);
 	
 	if(rank==0){
 		cout << endl<<"EL Grafo con las distancias de los caminos más cortos es:"<<endl<<endl;
